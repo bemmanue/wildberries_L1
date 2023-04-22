a4a8ecdc0dc021e151cd3d1d900a4fa7892a0c26
bemmanue <myaushenka@gmail.com>
2023-04-22 20:23:38 +0300

    done

--- ex03/main.go
+++ ex03/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+// square записывает в канал квадрат числа
+func square(n int, ch chan<- int) {
+	ch <- n * n
+}
+
+func main() {
+	// создаем и инициализируем массив
+	arr := [5]int{2, 4, 6, 8, 10}
+
+	// создаем буферизованный канал
+	ch := make(chan int, len(arr))
+
+	// создаем переменную для записи суммы квадратов чисел
+	var result int
+
+	// запускаем горутины, которые будут записывать квадрат числа в канал
+	for _, n := range arr {
+		go square(n, ch)
+	}
+
+	// читаем из канала результаты работы горутин
+	for i := 0; i < cap(ch); i++ {
+		result += <-ch
+	}
+
+	// закрываем канал
+	close(ch)
+
+	// печатаем результат
+	fmt.Println(result)
 }
