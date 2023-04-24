7108a42ced748ee0291defa7663384d05129cb41
bemmanue <myaushenka@gmail.com>
2023-04-24 21:29:30 +0300

    fix

--- ex23/main.go
+++ ex23/main.go
@@ -9,17 +9,13 @@ func remove(arr []int, i int) []int {
 		return arr
 	}
 
-	// создаем новый срез и копируем в него исходный, чтобы у каждого была своя область памяти
-	arr2 := make([]int, len(arr))
-	copy(arr2, arr)
-
-	return append(arr2[:i], arr2[i+1:]...)
+	return append(arr[:i], arr[i+1:]...)
 }
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5} // срез
 	i := 2                      // индекс элемента, который нужно удалить из среза
 
-	fmt.Println(remove(arr, i)) // печатаем срез с удаленным элементом
-	fmt.Println(arr)            // изменения не отразились на исходном срезе
+	arr = remove(arr, i)
+	fmt.Println(arr)
 }
