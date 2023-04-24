664e4f64208b94e594d0b7354c2e36cdb4577234
bemmanue <myaushenka@gmail.com>
2023-04-24 06:11:04 +0300

    done

--- ex19/main.go
+++ ex19/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func reverse(s string) string {
+	r := []rune(s)
+
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	return string(r)
+}
+
+func main() {
+	s := "главрыба"
+
+	fmt.Printf("%s – %s\n", s, reverse(s))
 }
